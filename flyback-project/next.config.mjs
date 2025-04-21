d3bbab42134f1a2be1e92e1278ee5e9b173b264c
alexabreup <eletrolabelt@gmail.com>
2025-04-21 02:32:24 -0300

    Configurar Next.js para GitHub Pages

--- flyback-project/next.config.mjs
+++ flyback-project/next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ic2025_alexandre-pereira',
+  basePath: '/ic2025_alexandre-pereira/flyback-project',
+  assetPrefix: '/ic2025_alexandre-pereira/flyback-project/',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
   },
 }
 
