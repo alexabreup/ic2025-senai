599c41498fed9681bb339d1c0e0daa02c66ed843
alexabreup <eletrolabelt@gmail.com>
2025-05-14 19:43:21 -0300

    back

--- flyback-project/app/layout.tsx
+++ flyback-project/app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Roboto } from "next/font/google"
+import { Roboto, Roboto_Mono } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/navbar"
@@ -12,10 +12,17 @@ const roboto = Roboto({
   variable: "--font-roboto",
 })
 
+const robotoMono = Roboto_Mono({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto-mono",
+})
+
 export const metadata: Metadata = {
   title: "IA no Controle e Predição de Falhas em Fonte Chaveada Flyback",
   description:
     "Projeto de Iniciação Científica desenvolvido na Faculdade de Tecnologia em Eletrônica Industrial do Senai Anchieta",
+    generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -24,8 +31,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
-      <body className={`${roboto.variable} font-sans`}>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={`${roboto.variable} ${robotoMono.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <div className="flex min-h-screen flex-col">
             <Navbar />
